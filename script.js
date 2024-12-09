8b0e86f7c1ab11d5a4ccd572c6cadbfd219eb54b
sumit-2002 <144438202+sumit-2002@users.noreply.github.com>
2024-12-09 12:31:18 +0530

    ...

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // When the user scrolls down 20px from the top of the document, slide down the navbar
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -7,4 +8,24 @@ function scrollFunction() {
   } else {
     document.getElementById("navbar").style.top = "0px";
   }
+
+
+
+
 }
+
+const menulink = document.querySelector(".menulink");
+const menutoggle = document.querySelector(".toggle");
+menutoggle.onclick = function () {
+  console.log("click");
+
+  menutoggle.classList.toggle("active");
+
+  if (menulink.style.right === "0px") {
+    menulink.style.right = "-550px";
+  } else {
+    menulink.style.right = "0px";
+  }
+};
+
+
